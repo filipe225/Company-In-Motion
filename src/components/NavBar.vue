6fc35a78bf01fb5809a52a8858e470af4a7700ec
hadespower@gmail.com <filipesousa@cloudworks.pt>
2018-11-06 11:09:04 +0000

    sidemenu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-toolbar>
-    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
-    <v-toolbar-title>Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat><router-link to="/">Home</router-link></v-btn>
-      <v-btn flat><router-link to="/timeline">TimeLine</router-link></v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+    <div>
+        <v-navigation-drawer v-model="sideNav">
+            <v-list>
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>supervisor_account</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>Home</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>supervisor_account</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>Timeline</v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar>
+            <v-toolbar-side-icon  @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+            <v-toolbar-title>Title</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat><router-link to="/">Home</router-link></v-btn>
+                <v-btn flat><router-link to="/timeline">TimeLine</router-link></v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </div>
 </template>
 
 <script>
@@ -15,7 +33,7 @@ export default {
     name: 'navbar',
     data: function() {
         return {
-
+            sideNav: false
         }
     }
 }
