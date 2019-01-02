e15a909e678ec876a21c8be6244a7a6e44afd614
hadespower@gmail.com <filipesousa@cloudworks.pt>
2019-01-02 18:06:08 +0000

    a

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,63 +1,65 @@
 <template>
+  <v-card transition="slide-x-reverse-transition">
+    <v-toolbar color="orange darken-2" dark>
+      <v-toolbar-title>Tasks</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>add_circle</v-icon>
+      </v-btn>
+    </v-toolbar>
 
-    <v-card transition="slide-x-reverse-transition">
-        
-        <v-toolbar color="orange darken-2" dark>
-            <v-toolbar-title>Tasks</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon><v-icon>add_circle</v-icon></v-btn>
-        </v-toolbar>
+    <v-list one-line v-if="!items">
+      <v-list-tile>
+        <p class="text-xs-center">COMING SOON</p>
+      </v-list-tile>
+    </v-list>
 
-        <v-list two-line>
-          <template v-for="(item, index) in items">
-            <v-list-tile :key="item.title" avatar ripple>
-                <v-list-tile-content>
-                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    <v-list-tile-sub-title>{{ item.content }}</v-list-tile-sub-title>
-                </v-list-tile-content>
+    <v-list two-line>
+      <template v-for="(item, index) in items">
+        <v-list-tile :key="item.title" avatar ripple>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ item.content }}</v-list-tile-sub-title>
+          </v-list-tile-content>
 
-                <v-list-tile-action>
-                    <v-list-tile-action-text>{{ item.date }}</v-list-tile-action-text>
-                    <v-list-tile-action-text>{{ item.deadline }}</v-list-tile-action-text>
-                    <v-list-tile-action-text>{{ item.status }}</v-list-tile-action-text>
-                </v-list-tile-action>
-
-            </v-list-tile>
-            <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
-          </template>
-        </v-list>
-
-    </v-card>
-
+          <v-list-tile-action>
+            <v-list-tile-action-text>{{ item.date }}</v-list-tile-action-text>
+            <v-list-tile-action-text>{{ item.deadline }}</v-list-tile-action-text>
+            <v-list-tile-action-text>{{ item.status }}</v-list-tile-action-text>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+      </template>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
 export default {
-    props: ['userAssignedTasks'],
+  props: ["userAssignedTasks"],
 
-    data() {
-        return {};
-    },
+  data() {
+    return {};
+  },
 
-    computed: {
-        items () {
-            return this.userAssignedTasks;
-        }
-    },
-
-    created: function() {
-    },
-
-    methods: {
-        toggle(index) {
-            const i = this.selected.indexOf(index);
-
-            if (i > -1) {
-            this.selected.splice(i, 1);
-            } else {
-            this.selected.push(index);
-            }
-        }
+  computed: {
+    items() {
+      return this.userAssignedTasks;
     }
+  },
+
+  created: function() {},
+
+  methods: {
+    toggle(index) {
+      const i = this.selected.indexOf(index);
+
+      if (i > -1) {
+        this.selected.splice(i, 1);
+      } else {
+        this.selected.push(index);
+      }
+    }
+  }
 };
 </script>
